4ead9b99f2df4b6b1f5e9a723da27af208ce1349
rinjumanandhar <you@example.com>
2018-11-17 23:07:15 +0545

    schema defined for mongoose mongodb

---
+++ model/model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+//Connect to mongoDb
+mongoose.connect('mongodb://rinju:nepal4321@ds024748.mlab.com:24748/mydb');
+
+//Create Schema and Model
+
+const Schema = mongoose.Schema;
+ 
+const mySchema = new Schema({
+	
+	username: {type: String, required: true, max: 100},
+    age: {type: Number, required: true},
+    date: {type: Date,default: Date.now()}
+});
+
+const MyModel = mongoose.model('MyModel', mySchema);
+module.exports = MyModel;
+
